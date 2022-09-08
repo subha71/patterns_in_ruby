a7b5733b94fe73b941012ecc7ab20420dd9d0ba7
subha71 <46778968+subha71@users.noreply.github.com>
2022-09-08 09:33:27 +0530

    pattern_7

---
+++ pattern_7.rb
@@ -0,0 +1,14 @@
+puts "enter no of rows"
+rows = gets.chomp.to_i
+puts "enter no of columns"
+columns = gets.chomp.to_i
+
+for i in 1..rows
+    for j in columns.downto(i)
+        print "  "
+    end
+        for j in 1..2*i-1
+            print "*" +" "
+        end
+    puts
+end
