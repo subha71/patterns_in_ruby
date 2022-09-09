9f9a4409eeaa6259d1bce19baaa9b7238e19a511
subha71 <46778968+subha71@users.noreply.github.com>
2022-09-09 09:12:06 +0530

    pattern_8

--- pattern_8.rb
+++ pattern_8.rb
@@ -3,8 +3,8 @@ rows = gets.chomp.to_i
 puts "enter no of columns"
 columns = gets.chomp.to_i
 
-for i in 1..rows
-    for j in columns.downto(i)
+for i in rows.downto(1)
+   for j in columns.downto(i)
         print "  "
     end
         for k in 1..2*i-1
